d5a81679d15447931487ab086635654c93998e5b
AdmireSeven <AdmireSeven@163.com>
2021-09-18 10:02:19 +0800

    Space replacing for second submission

--- SpaceReplace.py
+++ SpaceReplace.py
@@ -8,27 +8,24 @@ import os
 import re
 reg = r"\be.*?f\b"
 #reg = r"\/\/.*"
-def replace_space(path):
+def replace_space(file_name):
     '''
     替换注释
-    :param path: 文件路径
+    :param file_name: 文件路径
     :return: 替换注释（字符串）后的str
     '''
     ls = []
-    file_list = os.listdir(path)
-    # 将输入的路径文件夹内所有文件的内容读取到text中
-    for count in range(len(file_list)):
-        file_name = path + '/' + file_list[count]
-        if file_name.endswith('.cpp'):
-            #print(file_name)
-            fr = open(file_name, 'r', encoding='UTF-8')
-            for line in fr:
+
+    if file_name.endswith('.cpp') or file_name.endswith('.c'):
+        #print(file_name)
+        fr = open(file_name, 'r', encoding='UTF-8')
+        for line in fr:
+            #print(line)
+            #if (line != None):
                 #print(line)
-                #if (line != None):
-                 #   print(line)
-                    #ls.extend(line)  # 将多个列表合成一个列表
-                ls.append(line)
-            fr.close()
+                #ls.extend(line)  # 将多个列表合成一个列表
+            ls.append(line)
+        fr.close()
     text = "".join(ls)
 
     comment = re.finditer(reg,text)
