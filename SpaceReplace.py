da5d3820c321e5bfb0e5b7ab893356d3ce2c6786
AdmireSeven <AdmireSeven@163.com>
2021-09-18 20:27:27 +0800

    Space replacement for the third submission

--- SpaceReplace.py
+++ SpaceReplace.py
@@ -3,31 +3,15 @@ author: Yaqi Zhang
 Function: Replace the space between 'else' and 'if'
 Email: admireseven@163.com
 '''
-#此题需求：字符串与注释一并替换
-import os
 import re
 reg = r"\be.*?f\b"
 #reg = r"\/\/.*"
-def replace_space(file_name):
+def replace_space(text):
     '''
     替换注释
-    :param file_name: 文件路径
+    :param text: 字符串
     :return: 替换注释（字符串）后的str
     '''
-    ls = []
-
-    if file_name.endswith('.cpp') or file_name.endswith('.c'):
-        #print(file_name)
-        fr = open(file_name, 'r', encoding='UTF-8')
-        for line in fr:
-            #print(line)
-            #if (line != None):
-                #print(line)
-                #ls.extend(line)  # 将多个列表合成一个列表
-            ls.append(line)
-        fr.close()
-    text = "".join(ls)
-
     comment = re.finditer(reg,text)
     for match in comment:
         #print(match.group())
